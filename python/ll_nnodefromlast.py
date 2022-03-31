11e668ff9ea96f5cc2504e986b816aa7433468ed
ajit-000 <ajitagnihotri000@gmail.com>
2022-04-01 00:28:10 +0530

    init

---
+++ python/ll_nnodefromlast.py
@@ -0,0 +1,41 @@
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+
+class LL:
+    def __init__(self):
+        self.head = None
+
+    def printList(self):
+        temp = self.head
+        while (temp):
+            print(temp.data, end=" ")
+            temp = temp.next
+
+    def nth_node(self, node):
+        prev, curr = self.head, self.head
+        for i in range(node):
+            curr = curr.next
+        while curr != None:
+            prev = prev.next
+            curr = curr.next
+        print("\n",prev.data)
+
+
+if __name__ == "__main__":
+    link = LL()
+    link.head = Node(input())
+    sec = Node(input())
+    th = Node(input())
+    fr = Node(input())
+    fv = Node(input())
+    link.head.next = sec
+    sec.next = th
+    th.next = fr
+    fr.next = fv
+    link.printList()
+    print()
+    link.nth_node(
+        int(input("Enter Position :")))
