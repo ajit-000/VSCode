8dec329e77e93521babc55c208ebbb0c5c18300d
ajit-000 <ajitagnihotri000@gmail.com>
2022-03-31 19:21:27 +0530

    init

---
+++ python/ll_mid.py
@@ -0,0 +1,33 @@
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+
+class LL:
+    def __init__(self):
+        self.head = None
+
+    def printList(self):
+        temp = self.head
+        while (temp):
+            print(temp.data, end=" ")
+            temp = temp.next
+
+    def midll(self):
+        headnode = self.head
+        cnt = 0
+        while headnode.next != None:
+            cnt += 1
+        print(cnt)
+
+
+if __name__ == "__main__":
+    link = LL()
+    link.head = Node(1)
+    sec = Node(2)
+    th = Node(3)
+    link.head.next = sec
+    sec.next = th
+    link.printList()
+    link.midll()
