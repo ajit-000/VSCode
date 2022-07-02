ce3e7d6497c948eab5d4623581931e0604acd41b
ajit-000 <ajitagnihotri000@gmail.com>
2022-07-02 15:48:03 +0530

    init

--- python/runnerup.py
+++ python/runnerup.py
@@ -1,25 +1,22 @@
-# total = {}
+total = {}
 
 
-# def insert(items):
-#     if items in total:
-#         total[items] += 1
-#     else:
-#         total[items] = 1
+def insert(items):
+    if items in total:
+        total[items] += 1
+    else:
+        total[items] = 1
 
 
-# insert('Apple')
-# insert('Ball')
-# insert('Apple')
-# print(len(total))
-# print(total)
-# L1 = list()
+insert('Apple')
+insert('Ball')
+insert('Apple')
+print(len(total))
+print(total)
+L1 = list()
 
 
-# L1.append([1, [2, 3], 4])
-# L1.extend([7, 8, 9])
-# print(L1)
-# print(L1[0][1][1] + L1[2])
-
-s = {abs}
-print(s)
+L1.append([1, [2, 3], 4])
+L1.extend([7, 8, 9])
+print(L1)
+print(L1[0][1][1] + L1[2])
