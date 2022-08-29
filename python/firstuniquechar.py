b4cb6e72c136f875b8802bf97e5b20d1ac32c291
ajit-000 <ajitagnihotri000@gmail.com>
2022-08-30 01:21:03 +0530

    init

--- python/firstuniquechar.py
+++ python/firstuniquechar.py
@@ -1,14 +1,18 @@
+from collections import Counter
+
+
 def check(s):
-    d = {}
-    for i in s:
-        d[i] = d.get(i, 0)+1
-    lst = []
+    lst=[]
+    d = Counter(s)
     for i, j in d.items():
         if j == 1:
             lst.append(i)
+            break
     if len(lst) == 0:
         return -1
-    return lst
+    for i in range(len(s)):
+        if s[i]==lst[0]:
+            return i+1
 
 
 if __name__ == "__main__":
