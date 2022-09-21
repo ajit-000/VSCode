41aa8af744c2613a160e4946398d788c1670536c
ajit-000 <ajitagnihotri000@gmail.com>
2022-09-21 14:34:36 +0530

    init

--- python/lis.py
+++ python/lis.py
@@ -16,17 +16,15 @@
 #     print(lis(nums))
 
 def lis(arr):
-    n=len(arr)
-    dp=[1]*n
-    for i in range(1,n):
-        res=0
+    n = len(arr)
+    dp = [1]*n
+    for i in range(1, n):
         for j in range(i):
-            if arr[i]>arr[j]:
-                if dp[j]>res:
-                    res=dp[j]
-        dp[i]=res+1
+            if arr[i] > arr[j] and dp[j] >= dp[i]:
+                dp[i] = 1+dp[j]
     print(dp)
 
-if __name__=="__main__":
-    arr=list(map(int,input().split()))
+
+if __name__ == "__main__":
+    arr = list(map(int, input().split()))
     lis(arr)
